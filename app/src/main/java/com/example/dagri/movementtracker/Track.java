86c83be706dd3b870e74504cb323146043256d0e
DaGri <david.griegel.1991@gmail.com>
2016-06-08 12:48:52 +0200

    Update
    TODOS geupdated

--- app/src/main/java/com/example/dagri/movementtracker/Track.java
+++ app/src/main/java/com/example/dagri/movementtracker/Track.java
@@ -26,6 +26,11 @@ public class Track {
      */
     private ArrayList<Double> speeds = new ArrayList();
 
+    /**
+     * The length of this Track.
+     */
+    private double distance = 0.0;
+
     // CONSTRUCTORS
 
     /**
@@ -67,6 +72,22 @@ public class Track {
      */
     public void addLatLng(double lat, double lon) {
         this.getLatLngs().add(new LatLng(lat, lon));
+    }
+
+    /**
+     * Calculates the length of this Track in meters.
+     */
+    public void calcLength(){
+        // RESET THE DISTANCE
+        this.setDistance(0.0);
+        // ITERATE OVER THE INTERNAL STORED POINTS
+        for(int a = 0; a < this.getLatLngs().size(); a++){
+            // CALCULATE THE DISTANCE BETWEEN TWO POINTS
+            // TODO : FILL
+            double add = 0.0;
+            // UPDATE THE DISTANCE
+            this.setDistance(this.getDistance() + add);
+        }
     }
 
     // GETTERS AND SETTERS
@@ -117,5 +138,21 @@ public class Track {
      */
     public void setSpeeds(ArrayList<Double> speeds) {
         this.speeds = speeds;
+    }
+
+    /**
+     * Returns the lenght of this track in meters.
+     * @return
+     */
+    public double getDistance() {
+        return distance;
+    }
+
+    /**
+     * Sets the length of this Track in meters.
+     * @param distance
+     */
+    public void setDistance(double distance) {
+        this.distance = distance;
     }
 }
